ccfa14463328ed25946ff78baffa011db24252b8
beaverbae2 <qogustjr119@naver.com>
2021-11-27 15:26:49 +0900

    feat : 프로젝트 초기 설정 - JPA 의존성은 driver 없으면 오류발생하여 일단 주석처리

---
+++ src/main/java/com/shackshake/fo4squardmaker/Fo4SquardMakerApplication.java
@@ -0,0 +1,13 @@
+package com.shackshake.fo4squardmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fo4SquardMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fo4SquardMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shackshake/fo4squardmaker/Fo4SquardMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shackshake.fo4squardmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fo4SquardMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
