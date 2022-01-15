420e29ef7b951956228548648ab24e73d0510282
beaverbae2 <qogustjr119@naver.com>
2022-01-16 01:27:15 +0900

    feat : SeasonRepository 생성

---
+++ src/main/java/com/shackshake/fo4squardmaker/repository/SeasonRepository.java
@@ -0,0 +1,8 @@
+package com.shackshake.fo4squardmaker.repository;
+
+import com.shackshake.fo4squardmaker.entity.Season;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeasonRepository extends JpaRepository<Season, Long> {
+
+}
