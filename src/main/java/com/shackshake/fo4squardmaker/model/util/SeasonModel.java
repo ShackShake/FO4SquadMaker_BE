ace5f03df7ce0709805f5c4fdd3089185d46fe5d
beaverbae2 <qogustjr119@naver.com>
2022-01-16 01:26:41 +0900

    feat : SeasonModel 생성
    - 인벤에서 크롤링한 season_id와 season_name 데이터를 담는 용도

---
+++ src/main/java/com/shackshake/fo4squardmaker/model/util/SeasonModel.java
@@ -0,0 +1,12 @@
+package com.shackshake.fo4squardmaker.model.util;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter @ToString
+@NoArgsConstructor
+public class SeasonModel {
+    private Long seasonId;
+    private String className;
+}
